294d61e78e3f1c0e447c7fdfc9e8c46e194fce7e
shubh dubey <shubhdubey8756@gmail.com>
2026-06-16 21:49:43 +0530

    ✅ Revising the Select Query I [db2] — approach1

---
+++ solutions/db2/easy/revising-the-select-query/solution-approach1.sql
@@ -0,0 +1,12 @@
+-- ──────────────────────────────────────────────────
+-- Problem     Revising the Select Query I
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    db2
+-- Status      Accepted
+-- Submitted   2026-06-16, 09:49 p.m.
+-- ──────────────────────────────────────────────────
+
+
+SELECT * FROM CITY WHERE POPULATION >100000 AND COUNTRYCODE ='USA';
