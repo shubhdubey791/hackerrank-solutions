9a24c6dfb03d01dbe6707bd2472b9788d10fd629
shubh dubey <shubhdubey8756@gmail.com>
2026-06-24 20:42:36 +0530

    ✅ Japanese Cities' Names [mysql] — approach1

---
+++ solutions/mysql/easy/japanese-cities-name/solution-approach1.sql
@@ -0,0 +1,13 @@
+-- ──────────────────────────────────────────────────
+-- Problem     Japanese Cities' Names
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-06-24, 08:42 p.m.
+-- ──────────────────────────────────────────────────
+
+select name
+from city
+where countrycode ='jpn';
