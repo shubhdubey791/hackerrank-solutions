14273f867395c569c7dfbd0ce9b19990c8c4f22b
shubh dubey <shubhdubey8756@gmail.com>
2026-06-24 20:46:01 +0530

    ✅ Select By ID [mysql] — approach1

---
+++ solutions/mysql/easy/select-by-id/solution-approach1.sql
@@ -0,0 +1,13 @@
+-- ──────────────────────────────────────────────────
+-- Problem     Select By ID
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-06-24, 08:45 p.m.
+-- ──────────────────────────────────────────────────
+
+select *
+from city 
+where id = 1661;
