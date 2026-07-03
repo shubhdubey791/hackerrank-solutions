b1935fef468fac7c0aee7f957f5c0f4953926cd8
shubh dubey <shubhdubey8756@gmail.com>
2026-07-03 11:42:32 +0530

    ✅  Java Stdin and Stdout I [java8] — approach1

---
+++ solutions/java8/easy/java-stdin-and-stdout-1/solution-approach1.java
@@ -0,0 +1,27 @@
+// ──────────────────────────────────────────────────
+// Problem      Java Stdin and Stdout I
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    java8
+// Status      Accepted
+// Submitted   2026-07-03, 11:42 a.m.
+// ──────────────────────────────────────────────────
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+          int b = scan.nextInt();
+          int c = scan.nextInt();
+        
+      
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        
+    }
+}
